64adc525a05a4b53ee237ccfd9dc8044f0f800bb
Volodymyr Gergel <volodymyr_gergel_tp@bmc.com>
2016-03-15 12:13:22 +0200

    test 15

--- src/test/JavaCoreTraining/TwoArray_noMatrix.java
+++ src/test/JavaCoreTraining/TwoArray_noMatrix.java
@@ -8,17 +8,17 @@ import java.util.Formatter;
 public class TwoArray_noMatrix {
 
     public static void main(String[] args) {
-        final int columnsNumber = 5;
+        final int columnsNumber = 15;
  //       Formatter fmt = new Formatter();
-            for (int value = 1; value <= 25; value++) {
+            for (int value = 1; value <= 225; value++) {
             int index = value - 1;
             int columnIndex  = index % columnsNumber;
             int rowIndex  = index / columnsNumber;
             boolean isRightDiagonal = rowIndex + columnIndex == columnsNumber - 1;
             if (rowIndex == columnIndex || isRightDiagonal) {
-                System.out.print("*\t");
+                System.out.format(" *\t");
             } else {
-                System.out.print(value + "\t");
+                System.out.format(value + "\t");
             }
             if (value % columnsNumber == 0) {
                 System.out.println();
